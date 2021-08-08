e1ef016fea7f8266dcc062d1db6d326b67171f6f
halmk <elle2206@i.softbank.jp>
2021-08-08 22:19:09 +0900

    #36 Create lambda_function.py

---
+++ aws/lambda/scale-clip/lambda_function.py
@@ -0,0 +1,31 @@
+import os
+import json
+import boto3
+import time
+import sys
+import logging
+import pymysql
+
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+# RDS settings
+RDS_HOST = os.environ('RDS_HOST')
+RDS_USER = os.environ('RDS_USER')
+RDS_PASSWORD = os.environ('RDS_PASSWORD')
+RDS_NAME = os.environ('RDS_NAME')
+
+try:
+    conn = pymysql.connect(host=RDS_HOST, user=RDS_USER, passwd=RDS_PASSWORD, db=RDS_NAME, connect_timeout=5)
+except pymysql.MySQLError as e:
+    logger.error("ERROR: Unexpected error: Could not connect to MySQL instance.")
+    logger.error(e)
+    sys.exit()
+
+logger.info("SUCCESS: Connection to RDS MySQL instance succeeded")
+
+
+def lambda_handler(event, context):
+    print(event['Records'][0]['body'])
+    message = event['Records'][0]['body']
