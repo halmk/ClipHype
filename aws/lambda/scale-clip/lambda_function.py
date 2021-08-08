5417c5872d9dd90a87a6d5572df67bf959a37aa1
halmk <elle2206@i.softbank.jp>
2021-08-09 01:32:12 +0900

    #36 Add 環境変数の取得の修正とオブジェクトのダウンロード

--- aws/lambda/scale-clip/lambda_function.py
+++ aws/lambda/scale-clip/lambda_function.py
@@ -11,13 +11,13 @@ logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 # RDS settings
-RDS_HOST = os.environ('RDS_HOST')
-RDS_USER = os.environ('RDS_USER')
-RDS_PASSWORD = os.environ('RDS_PASSWORD')
-RDS_NAME = os.environ('RDS_NAME')
+RDS_HOST = os.environ['RDS_HOST']
+RDS_USER = os.environ['RDS_USER']
+RDS_PASSWORD = os.environ['RDS_PASSWORD']
+RDS_NAME = os.environ['RDS_NAME']
 
 try:
-    conn = pymysql.connect(host=RDS_HOST, user=RDS_USER, passwd=RDS_PASSWORD, db=RDS_NAME, connect_timeout=5)
+    conn = pymysql.connect(host=RDS_HOST, user=RDS_USER, passwd=RDS_PASSWORD, db=RDS_NAME, connect_timeout=2)
 except pymysql.MySQLError as e:
     logger.error("ERROR: Unexpected error: Could not connect to MySQL instance.")
     logger.error(e)
@@ -27,5 +27,33 @@ logger.info("SUCCESS: Connection to RDS MySQL instance succeeded")
 
 
 def lambda_handler(event, context):
-    print(event['Records'][0]['body'])
-    message = event['Records'][0]['body']
+    # TODO implement
+    logger.info(event['Records'][0]['s3']['object']['key'])
+    key = event['Records'][0]['s3']['object']['key']
+    task_id = key.split('_')[1].split('.')[0]
+    logger.info(task_id)
+
+    with conn.cursor() as cur:
+        sql = 'SELECT * FROM `app_digest` WHERE `task_id`=%s'
+        cur.execute(sql, (task_id,))
+        result = cur.fetchone()
+        print(result)
+
+
+
+    client = boto3.client('s3')
+    response = client.get_object(
+        Bucket=task.bucket,
+        Key=key
+    )
+    body = response['Body'].read()
+
+    with open('clip.mp4', 'wb') as f:
+        f.write(body)
+
+
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
