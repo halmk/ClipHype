007e0fc8b6e42a3522fd9c77174cf95f3d66d7bc
halmk <elle2206@i.softbank.jp>
2021-08-14 03:00:43 +0900

    Bug fix

--- cliphype/cliphype/settings.py
+++ cliphype/cliphype/settings.py
@@ -27,10 +27,9 @@ env.read_env('.env')
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DEBUG')
+DEBUG = env.bool('DEBUG', default=False)
 
-
-ALLOWED_HOSTS = env('ALLOWED_HOSTS')
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 
 # Other Service Keys
 # AWS
