3dab5a1c3ae2ee271284d45629456301152c78d1
halmk <elle2206@i.softbank.jp>
2021-08-17 02:41:27 +0900

    #28 Mod RDS連携をなくしてs3から情報を取得

--- aws/lambda/scale-clip/lambda_function.py
+++ aws/lambda/scale-clip/lambda_function.py
@@ -4,87 +4,61 @@ import boto3
 import time
 import sys
 import logging
-import pymysql.cursors
 import subprocess
 
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-# RDS settings
-RDS_HOST = os.environ['RDS_HOST']
-RDS_USER = os.environ['RDS_USER']
-RDS_PASSWORD = os.environ['RDS_PASSWORD']
-RDS_NAME = os.environ['RDS_NAME']
-
-try:
-    conn = pymysql.connect(
-        host=RDS_HOST,
-        user=RDS_USER,
-        passwd=RDS_PASSWORD,
-        db=RDS_NAME,
-        cursorclass=pymysql.cursors.DictCursor,
-        connect_timeout=5,
-    )
-except pymysql.MySQLError as e:
-    logger.error("ERROR: Unexpected error: Could not connect to MySQL instance.")
-    logger.error(e)
-    sys.exit()
-
-logger.info("SUCCESS: Connection to RDS MySQL instance succeeded")
-
 
 def lambda_handler(event, context):
     logger.info(event['Records'][0]['s3']['object']['key'])
+
+    # get parameters from s3 object key triggered
+    bucket_name = event['Records'][0]['s3']['bucket']['name']
     key = event['Records'][0]['s3']['object']['key']
     filename = key.split('/')[-1]
+    creator = filename.split('_')[0]
     task_id = key.split('_')[1].split('/')[0]
     logger.info(task_id)
 
-    with conn.cursor() as cur:
-        sql = 'SELECT `bucket`, `status`, `num_clips` FROM `app_digest` WHERE `task_id`=%s'
-        cur.execute(sql, (task_id))
-        task = cur.fetchone()
-        logger.info(task)
+    s3 = boto3.resource('s3')
+    bucket = s3.Bucket(bucket_name)
 
+    s3_client = boto3.client('s3')
+
+    json_key = f'digest/info/{creator}/{task_id}.json'
+    response = s3_client.get_object(
+        Bucket=bucket_name,
+        Key=json_key
+    )
+    data = response['Body'].read()
+    data = data.decode('utf-8')
+    data = json.loads(data)
+    logger.info(data)
+
+    # download the triggered clip
     os.makedirs('/tmp/src/', exist_ok=True)
     os.makedirs('/tmp/out/', exist_ok=True)
     clip_path = f'/tmp/src/{filename}'
 
-    s3 = boto3.resource('s3')
-    bucket = s3.Bucket(task['bucket'])
     logger.info(f'Downloding clip...: {clip_path}, {key}')
     bucket.download_file(key, clip_path)
 
+    # process scale to the clip
     out_path = f'/tmp/out/{filename}'
     scale_args = ['ffmpeg', '-i', clip_path, '-r', '60', '-s', '1920x1080', out_path, '-y']
     logger.info(f'Scaling clip...')
     res = subprocess.call(scale_args)
 
-    out_key = f'digest/input/input_{task_id}/{filename}'
+    # upload the processed clip
+    if data['is_drawtext']:
+        out_key = f'digest/scaled/scaled_{task_id}/{filename}'
+    else:
+        out_key = f'digest/input/input_{task_id}/{filename}'
+
     logger.info(f'Uploading scaled clip...: {out_path}, {out_key}')
     bucket.upload_file(out_path, out_key)
-
-    input_prefix = f'digest/input/input_{task_id}/'
-    client = boto3.client('s3')
-    logger.info(f'List scaled clip: {task["bucket"]}, {input_prefix}')
-    scaled_objects = client.list_objects(
-        Bucket=task['bucket'],
-        Prefix=input_prefix
-    )
-    logger.info(scaled_objects)
-    num_scaled_clips = len(scaled_objects['Contents'])
-
-    status = f'Scaling clips {num_scaled_clips}/{task["num_clips"]}'
-    if num_scaled_clips == task["num_clips"]:
-        status = 'Request instance'
-
-    logger.info(f'Update task status: {status}')
-    with conn.cursor() as cur:
-        sql = 'UPDATE `app_digest` SET `status`=%s WHERE `task_id`=%s'
-        cur.execute(sql, (status, task_id))
-
-    conn.commit()
 
     return {
         'statusCode': 200,
