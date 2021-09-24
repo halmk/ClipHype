8a0b0aa003aecc7590cd6dd1f6bce921b4046260
halmk <elle2206@i.softbank.jp>
2021-09-24 09:10:35 +0900

    Mod テーブル作成操作を除外

--- docker_start.sh
+++ docker_start.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 service mysql start
-echo 'CREATE DATABASE cliphype;' | mysql -u root
+#echo 'CREATE DATABASE cliphype;' | mysql -u root
 cd cliphype/
 export DJANGO_SETTINGS_MODULE=cliphype.local_settings
 nohup redis-server > ~/redis.log &
 nohup celery -A cliphype worker --loglevel=INFO --concurrency=1 > ~/celery.log &
 python3 manage.py makemigrations
 python3 manage.py migrate
-/bin/bash
+#/bin/bash
